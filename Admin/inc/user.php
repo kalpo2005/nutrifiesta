c41dbbdd6d59f7a864d8ca76b22191ff7712a4e1
Kalpesh Bavaliya <kalpeshbavaliya2005@gmail.com>
2025-04-09 23:29:06 +0530

    final frame

--- Admin/inc/user.php
+++ Admin/inc/user.php
@@ -81,7 +81,7 @@ $adminName = $_SESSION['admin_name'];
                 </div>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
+                    <a class="nav-link" href="user.php">
                         <span class="icon" data-bs-toggle="tooltip" data-bs-title="Users"><i class="fas fa-users"></i></span>
                         <span class="menu">Users</span>
                     </a>
@@ -98,9 +98,56 @@ $adminName = $_SESSION['admin_name'];
         </div>
         <!-- Sidebar end -->
 
-        <!-- main conent hear -->
+        <div class="content">
+            <!-- Top navbar -->
+            <nav class="navbar navbar-expand-md navbar-light bg-light shadow">
+                <div class="container-fluid px-3">
+                    <button class="navbar-toggler border-0" type="button" id="show_sidebar_phone">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-        
+                    <a class="navbar-brand d-none d-md-block" href="javascript:void(0)" id="show_sidebar_pc">
+                        <i class="fas fa-bars fa-lg"></i>
+                    </a>
+
+                    <div class="fw-bold text-secondary d-md-none d-block">Admin Panel</div>
+
+                    <div class="ms-auto d-flex align-items-center">
+                        <div class="nav-item d-none d-md-block me-2" data-bs-toggle="tooltip" data-bs-title="Full Screen" data-bs-placement="left">
+                            <a href="#" class="nav-link" id="fullscreen">
+                                <i class="fa-solid fa-expand"></i>
+                            </a>
+                        </div>
+
+                        <div class="dropdown">
+                            <a class="nav-link dropdown-toggle py-1 px-3 rounded-1" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-user-circle me-1"></i>
+                                Welcome, <?php echo htmlspecialchars($adminName); ?> 👋
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="#"><i class="fa-solid fa-address-card me-2"></i>Profile</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="fa-solid fa-gear me-2"></i>Account</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="logout.php"><i class="fa-solid fa-right-from-bracket me-2"></i>Logout</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </nav>
+
+            <!-- Main content -->
+           
+
+            <!-- Footer -->
+            <footer class="bg-light shadow text-secondary text-center d-flex flex-column flex-md-row justify-content-between p-3 p-md-4">
+                <div>Copyright &copy; 2025 Vanita Solanki</div>
+                <div>Made with support</div>
+            </footer>
+        </div>
+    </div>
 
     <?php require('layout/deshboardlink.php'); ?>
 
