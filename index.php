4c9d1a0702f4570d6052bea61569c2f47c895bf3
Kalpesh Bavaliya <kalpeshbavaliya2005@gmail.com>
2025-04-10 08:08:57 +0530

    logout and user name done

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +9,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>NutriFiesta-home</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
@@ -16,7 +21,9 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
     <div class="container-fluid">
       <a class="navbar-brand me-5 fw-bold fs-3 " href="index.php">NutriFiesta</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -30,7 +37,8 @@
 
 
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+              data-bs-toggle="dropdown" aria-expanded="false">
               Product
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -56,12 +64,30 @@
         <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success me-lg-2 me-3" type="submit">Search</button>
-          <button type="button" class="btn btn-outline-dark shadow-none me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#loginModal">
-            Login
-          </button>
-          <button type="button" class="btn btn-outline-dark shadow-none " data-bs-toggle="modal" data-bs-target="#registerModal">
-            Register
-          </button>
+
+          <div class="d-flex align-items-center">
+            <?php if (isset($_SESSION['name'])): ?>
+              <div class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Welcome, <strong><?php echo htmlspecialchars($_SESSION['name']); ?></strong>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="userDropdown">
+                  <li><a href="php/logout_user.php" class="dropdown-item">Logout</a></li>
+                </ul>
+              </div>
+            <?php else: ?>
+              <button type="button" class="btn btn-outline-dark shadow-none me-lg-2 me-3"
+                data-bs-toggle="modal" data-bs-target="#loginModal">
+                Login
+              </button>
+              <button type="button" class="btn btn-dark shadow-none" data-bs-toggle="modal"
+                data-bs-target="#registerModal">
+                Register
+              </button>
+            <?php endif; ?>
+          </div>
+
+
         </form>
       </div>
     </div>
@@ -69,9 +95,8 @@
 
 
 
-  <!-- this is a login model -->
-
-  <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+  <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="loginModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <form method="POST" action="php/login.php">
@@ -79,7 +104,8 @@
             <h5 class="modal-title" id="loginModalLabel">
               <i class="bi bi-person-fill me-2"></i> User Login
             </h5>
-            <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal"
+              aria-label="Close"></button>
           </div>
 
           <div class="modal-body">
@@ -104,21 +130,23 @@
   </div>
 
 
-  <!-- Ragister model  -->
-  <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
+  <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="registerModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
-        <form id="registerForm"> <!-- function throught entry handle -->
+        <form id="registerForm">
           <div class="modal-header">
             <h5 class="modal-title d-flex align-items-center">
               <i class="bi bi-person-lines-fill me-2"></i> User Registration
             </h5>
-            <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal"
+              aria-label="Close"></button>
           </div>
 
           <div class="modal-body shadow" style="background:url('images/bg-img/111.jpg'); background-size: cover;">
             <span class="badge bg-light text-dark mb-3 text-wrap lh-base">
-              Note: Your details must match with your ID (Aadhar card, passport, etc) that will be required during check-in.
+              Note: Your details must match with your ID (Aadhar card, passport, etc) that will be
+              required during check-in.
             </span>
 
             <div class="container-fluid">
@@ -171,7 +199,6 @@
 
 
 
-  <!-- carousel -->
   <div class="containerfluid px-lg-4 mt-4">
     <div class="swiper swiper-container">
       <div class="swiper-wrapper">
@@ -191,8 +218,6 @@
 
     </div>
   </div>
-  <!-- Check availability -->
-
   <div class="container">
     <div class="row">
       <div class="col-lg-12 bg-white shadow p-3 rounded">
@@ -200,14 +225,6 @@
       </div>
     </div>
   </div>
-
-
-
-
-
-
-
-  <!-- Our Grocery product--->
   <h2 class="mt-2 pt-4 mb-4  fw-bold text-center fm-bold h-font">Our Grocery Product</h2>
   <div class="container">
     <div class="row">
@@ -444,8 +461,6 @@
   </div>
 
 
-  <!--- Testimonials -->
-
   <h2 class="mt-5 pt-4  fw-bold mb-4 text-center fw-bold h-font ">Testimonials</h2>
   <div class="container">
     <div class="swiper Swiper-testimonials">
@@ -532,21 +547,23 @@
   </div>
 
 
-  <!--Reacch Us  -->
   <h2 class="mt-5 pt-4 fw-bold mb-4 text-center fm-bold h-font">Reach Us</h2>
   <div class="container">
     <div class="row">
       <div class="col lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
-        <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31047.69556995658!2d72.11686662361357!3d21.75534370046627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395f5a7323e981b5%3A0xa5182291c8c086b3!2sSardar%20Nagar%2C%20Bhavnagar%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1741516825914!5m2!1sen!2sin" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31047.69556995658!2d72.11686662361357!3d21.75534370046627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395f5a7323e981b5%3A0xa5182291c8c086b3!2sSardar%20Nagar%2C%20Bhavnagar%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1741516825914!5m2!1sen!2sin"
+          loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
       <div class="col-lg-4 col-md-4">
         <div class="bg-white p-4 rounded mb-4">
           <h5>Call Us</h5>
-          <a href="tel: +919016835668 " class="d-inline-block mb-3 text-decoration-none text-dark">
+          <a href="tel: +919016835668 "
+            class="d-inline-block mb-3 text-decoration-none text-dark">
             <i class="bi bi-telephone-fill"></i> +919016835668
           </a>
           <br>
-          <a href="tel: +919016835668 " class="d-inline-block text-decoration-none text-dark">
+          <a href="tel: +919016835668 "
+            class="d-inline-block text-decoration-none text-dark">
             <i class="bi bi-telephone-fill"></i> +919016835668
           </a>
         </div>
@@ -570,7 +587,8 @@
             </span>
           </a>
           <br>
-          <a href="tel: +919016835668 " class="d-inline-block text-decoration-none text-dark">
+          <a href="tel: +919016835668 "
+            class="d-inline-block text-decoration-none text-dark">
           </a>
         </div>
       </div>
@@ -611,16 +629,9 @@
   </div>
   <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by NutriFiesta Grocery Web </h6>
 
-  <!-- this is a for only visite count  -->
-  <!-- <div class="text-center text-muted">
-    Visitor Count:
-    <strong>
-      <?php require('php/visitor_counter.php'); ?>
-    </strong>
-  </div> -->
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
   <script>
